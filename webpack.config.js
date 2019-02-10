f5e143bde0703dd663393cf41ace46247a438c41
Yang-Xavier <xavier_ybx@163.com>
2019-02-10 14:43:00 +0000

    It is the basic enviroment for web application development

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,52 @@
 
 const path = require('path');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
+
+module.exports = {
+    mode: 'development',
+    entry: ['webpack/hot/dev-server','./app/main.js'],
+    output: {
+        path: path.resolve(__dirname, 'assets'),
+        filename: 'bundle.js',
+        publicPath: '/assets/',
+        library: 'lib',
+        libraryTarget: "umd"
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                }) //must be this order
+
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('[name].css'),
+        new htmlWebpackPlugin({
+            title: 'My App',
+            filename: 'index.html'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ]
 
 }
+
+
